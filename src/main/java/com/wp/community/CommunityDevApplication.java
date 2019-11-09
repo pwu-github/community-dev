7004a8e250b0641e7ef653153e3677f5df397765
pwu <pwu@abcft.com>
2019-11-09 15:50:02 +0800

    初始化git

---
+++ src/main/java/com/wp/community/CommunityDevApplication.java
@@ -0,0 +1,13 @@
+package com.wp.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wp/community/CommunityDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wp.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
