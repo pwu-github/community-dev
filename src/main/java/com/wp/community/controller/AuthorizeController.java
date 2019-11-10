4737c3b3c020d6b11d5c7f023d2a982867a98e75
wupeng <726451064@qq.com>
2019-11-10 10:34:39 +0800

    [change]将一些参数放进配置文件中

--- src/main/java/com/wp/community/controller/AuthorizeController.java
+++ src/main/java/com/wp/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.wp.community.dto.AccessTokenDTO;
 import com.wp.community.dto.GitHubUser;
 import com.wp.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,15 +14,21 @@ public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("e75ecff3b22a6d0dea80");
-        accessTokenDTO.setClient_secret("706a81b9e67dd0557c13744a255f5ab90fde63eb");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GitHubUser user = githubProvider.getUser(accessToken);
