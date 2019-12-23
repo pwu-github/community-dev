058766336e20314ca0cb5d13ed7c4ddbe3bbf8bc
pwu <pwu@abcft.com>
2019-12-23 20:45:08 +0800

    [add]整合mybatis

--- src/main/java/com/wp/community/CommunityDevApplication.java
+++ src/main/java/com/wp/community/CommunityDevApplication.java
@@ -1,9 +1,11 @@
 package com.wp.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wp.community.mapper")
 public class CommunityDevApplication {
 
     public static void main(String[] args) {
